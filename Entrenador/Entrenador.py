9bd3f5ca0cd59ac086040a0d02fd72e33bb2a411
Abiel Sandoval <85966720+AbielSandoval@users.noreply.github.com>
2021-08-19 17:02:42 -0400

    Add files via upload

---
+++ Entrenador/Entrenador.py
@@ -0,0 +1,39 @@
+import cv2
+import os
+import numpy as np
+
+dataPath = 'C:/Users/HP 5540/Desktop/Programacion 3/Vision Computacional/Trabajo final Prog III/Vision_Computacional/IMG'
+peopleList = os.listdir(dataPath)
+print('Lista de personas: ', peopleList)
+
+labels = []
+facesData = []
+label = 0
+
+for nameDir in peopleList:
+	personPath = dataPath + '/' + nameDir
+	print('Leyendo las imágenes')
+
+	for fileName in os.listdir(personPath):
+		print('Rostros: ', nameDir + '/' + fileName)
+		labels.append(label)
+		facesData.append(cv2.imread(personPath+'/'+fileName,0))
+		image = cv2.imread(personPath+'/'+fileName,0)
+		cv2.imshow('image',image)
+		cv2.waitKey(10)
+	label = label + 1
+
+
+
+# Métodos para entrenar el reconocedor
+face_recognizer = cv2.face.EigenFaceRecognizer_create()
+
+
+# Entrenando el reconocedor de rostros
+print("Entrenando...")
+face_recognizer.train(facesData, np.array(labels))
+
+# Almacenando el modelo obtenido
+face_recognizer.write('modeloEigenFace.xml')
+
+print("Modelo almacenado...")
